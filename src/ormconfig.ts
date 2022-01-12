5e1631d8be25429e56d3e9567a9cb71ca0471c18
Nijat <nicatveliyev@gmail.com>
2022-01-13 03:37:06 +0400

    database

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,14 +1,15 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'fkstore',
-    password: 'n24i8cat',
-    database: 'fkinventory',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    // synchronize: true,
+  url: process.env.DATABASE_URL,
+  type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'fkstore',
+//   password: 'n24i8cat',
+//   database: 'fkinventory',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  // synchronize: true,
 };
 
 export default config;
