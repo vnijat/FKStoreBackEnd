fb2548ea5468d3a72dda827502b0e1b70632dacf
Nijat <nicatveliyev@gmail.com>
2022-01-13 07:10:18 +0400

    heroku test

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,17 +1,23 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-  url: process.env.DATABASE_URL,
+  //   url: process.env.DATABASE_URL,
   type: 'postgres',
-  ssl: {
-    rejectUnauthorized: false,
-  }, //   host: 'localhost',
-  //   port: 5432,
-  //   username: 'fkstore',
-  //   password: 'n24i8cat',
-  //   database: 'fkinventory',
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  host: 'localhost',
+  port: 5432,
+  username: 'fkstore',
+  password: 'n24i8cat',
+  database: 'fkinventory',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   // synchronize: true,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  migrationsRun: false,
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
 
 export default config;
